3e5a870019a1f101d8fa0fb502b9428f644fe9de
Ilmari Autio <ilmari.autio@gmail.com>
2015-10-31 23:06:57 +0200

    Update Polymer version and relocate custom type definitions

--- type_definitions/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="polymer/polymer.d.ts" />
-/// <reference path="webcomponents.js/webcomponents.js.d.ts" />
-/// <reference path="d3/d3.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
